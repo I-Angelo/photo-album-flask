e94bc7357148d2cf678f566699df79087da31229
I-Angelo <111244396+I-Angelo@users.noreply.github.com>
2023-02-27 15:47:55 -0500

    updates to code

--- app/static/images_carrousel/script.js
+++ app/static/images_carrousel/script.js
@@ -7,22 +7,6 @@ const interval = setInterval(function() {
 var index = 1;
 var start = 0;
 
-// startCarousel = () => {
-//     // if(start < images.length) {
-//     carousel.style.backgroundImage = `url(${images[index++]})`;
-//     carousel.classList.remove("fade");
-//     void carousel.offsetWidth;
-//     carousel.classList.add("fade");
-//     if(index > images.length -1) index - 0;
-//     // }
-// }
-
-// setInterval(startCarousel,2000)
-// while (start < images.length) {
-//     startCarousel()
-// }
-
-// /////////////
 
 function startCarousel(){
         if(start<images.length){
@@ -36,11 +20,9 @@ function startCarousel(){
         else{
             start=0;
         }
-        // console.log(img);
-        // img.innerHTML = "<img src="+slides[Start-1]+">";
-       
+               
     }
-    // setInterval(startCarousel,3000);
+    
 
 
 
@@ -55,28 +37,3 @@ function startCarousel(){
 
 
 
-
-// /////////////
-
-
-// var img = document.getElementById('img');
-
-// var slides = ["/static/images_carrousel/img1.jpeg", "/static/images_carrousel/img2.jpeg", "/static/images_carrousel/img3.jpeg", "/static/images_carrousel/img4.jpeg"];
-
-// var Start=0;
-
-// function slider(){
-//     if(Start<slides.length){
-//         Start=Start+1;
-//         img.classList.add("fade");
-//         void img.offsetWidth;
-//         img.classList.remove("fade");
-//     }
-//     else{
-//         Start=1;
-//     }
-//     console.log(img);
-//     img.innerHTML = "<img src="+slides[Start-1]+">";
-   
-// }
-// setInterval(slider,2000);
